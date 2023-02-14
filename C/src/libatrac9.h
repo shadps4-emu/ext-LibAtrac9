978dec28bc9023820a76f7f05a831f667f4ac0f9
Macdu <Macdu@users.noreply.github.com>
2023-02-14 16:09:46 +0100

    Fix static linking on MSVC

--- C/src/libatrac9.h
+++ C/src/libatrac9.h
@@ -3,16 +3,6 @@
 extern "C" {
 #endif
 
-#ifdef _MSC_VER
-#ifdef COMPILING_DLL 
-#define DLLEXPORT __declspec(dllexport)
-#else
-#define DLLEXPORT __declspec(dllimport)  
-#endif
-#else
-#define DLLEXPORT
-#endif
-
 #define ATRAC9_CONFIG_DATA_SIZE 4
 
 typedef struct {
@@ -26,13 +16,13 @@ typedef struct {
 	unsigned char configData[ATRAC9_CONFIG_DATA_SIZE];
 } Atrac9CodecInfo;
 
-DLLEXPORT void* Atrac9GetHandle(void);
-DLLEXPORT void Atrac9ReleaseHandle(void* handle);
+void* Atrac9GetHandle(void);
+void Atrac9ReleaseHandle(void* handle);
 
-DLLEXPORT int Atrac9InitDecoder(void* handle, unsigned char *pConfigData);
-DLLEXPORT int Atrac9Decode(void* handle, const unsigned char *pAtrac9Buffer, short *pPcmBuffer, int *pNBytesUsed);
+int Atrac9InitDecoder(void* handle, unsigned char *pConfigData);
+int Atrac9Decode(void* handle, const unsigned char *pAtrac9Buffer, short *pPcmBuffer, int *pNBytesUsed);
 
-DLLEXPORT int Atrac9GetCodecInfo(void* handle, Atrac9CodecInfo *pCodecInfo);
+int Atrac9GetCodecInfo(void* handle, Atrac9CodecInfo *pCodecInfo);
 
 #ifdef __cplusplus
 }
