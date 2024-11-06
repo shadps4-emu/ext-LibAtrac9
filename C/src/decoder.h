3acdcdc78f129c2e6145331ff650fa76dd88d62c
Vladislav Mikhalin <mikhalinvlad@gmail.com>
2024-11-06 18:50:12 +0300

    Add non-interleaved PCM output

--- C/src/decoder.h
+++ C/src/decoder.h
@@ -3,7 +3,7 @@
 #include "error_codes.h"
 #include "structures.h"
 
-At9Status Decode(Atrac9Handle* handle, const unsigned char* audio, unsigned char* pcm, int* bytesUsed);
-At9Status DecodeS32(Atrac9Handle* handle, const unsigned char* audio, int* pcm, int* bytesUsed);
-At9Status DecodeF32(Atrac9Handle* handle, const unsigned char* audio, float* pcm, int* bytesUsed);
+At9Status Decode(Atrac9Handle* handle, const unsigned char* audio, unsigned char* pcm, int* bytesUsed, int nointerleave);
+At9Status DecodeS32(Atrac9Handle* handle, const unsigned char* audio, int* pcm, int* bytesUsed, int nointerleave);
+At9Status DecodeF32(Atrac9Handle* handle, const unsigned char* audio, float* pcm, int* bytesUsed, int nointerleave);
 int GetCodecInfo(Atrac9Handle* handle, CodecInfo* pCodecInfo);
